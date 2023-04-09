2d2d653e4482264ce7a8abc38db66556962b78be
pacifi <pacifi.bnr@gmail.com>
2023-04-08 20:27:41 -0500

    gunicor

--- webapp/config/settings.py
+++ webapp/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nhr)g*yetkf9&t5c1u3k(k5jgc6^oe3t-m^)lj_m+50%*mc15*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
